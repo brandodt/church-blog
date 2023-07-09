13f48b0af0832bc66935599254bdcaca40d42f7e
Dwayne Lifter <67250900+dwayne-lifter@users.noreply.github.com>
2023-07-09 15:09:22 +0800

    added modal

--- html/script/main.js
+++ html/script/main.js
@@ -1,4 +1,61 @@
-// ANIMATION
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    // Dropdowns in navbar
+
+    var $dropdowns = getAll('.navbar-item.has-dropdown:not(.is-hoverable)');
+
+    if ($dropdowns.length > 0) {
+        $dropdowns.forEach(function ($el) {
+            $el.addEventListener('click', function (event) {
+                event.stopPropagation();
+                $el.classList.toggle('is-active');
+            });
+        });
+
+        document.addEventListener('click', function (event) {
+            closeDropdowns();
+        });
+    }
+
+    function closeDropdowns() {
+        $dropdowns.forEach(function ($el) {
+            $el.classList.remove('is-active');
+        });
+    }
+
+    // Close dropdowns if ESC pressed
+    document.addEventListener('keydown', function (event) {
+        var e = event || window.event;
+        if (e.keyCode === 27) {
+            closeDropdowns();
+        }
+    });
+
+    // Toggles
+
+    var $burgers = getAll('.burger');
+
+    if ($burgers.length > 0) {
+        $burgers.forEach(function ($el) {
+            $el.addEventListener('click', function () {
+                var target = $el.dataset.target;
+                var $target = document.getElementById(target);
+                $el.classList.toggle('is-active');
+                $target.classList.toggle('is-active');
+            });
+        });
+    }
+
+    // Functions
+
+    function getAll(selector) {
+        return Array.prototype.slice.call(document.querySelectorAll(selector), 0);
+    }
+});
+
+//fade animation example class="reveal fade-in"
 
 function reveal() {
     var reveals = document.querySelectorAll(".reveal");
@@ -16,14 +73,44 @@ function reveal() {
 
 window.addEventListener("scroll", reveal);
 
-// Restrict input to only numeric values
-document.getElementById('phone-input').addEventListener('input', function (e) {
-    e.target.value = e.target.value.replace(/\D/g, '');
+//modal for button
+
+// Function for opening the modal
+function popUp() {
+    // is-active class of the modal
+    document.getElementById("modal_id").classList.add("is-active");
+}
+// Adding event listeners for closing the modal
+document.querySelectorAll(
+    "#cancel"
+)
+    .forEach(($el) => {
+        const $modal = $el.closest(".modal");
+
+        $el.addEventListener("click", () => {
+            // removing is-active class from modal
+            $modal.classList.remove("is-active");
+        });
+    });
+
+//modal for cards
+
+document.querySelectorAll(".my-modal-trigger").forEach((card, index) => {
+    card.addEventListener("click", () => {
+        const modals = document.querySelectorAll(".my-modal");
+        const modal = modals[index];
+        modal.classList.add("is-active");
+        document.documentElement.classList.add("is-clipped"); // Add class to <html> element
+    });
 });
 
-// Prevent clearing of input on non-numeric keypress
-document.getElementById('phone-input').addEventListener('keypress', function (e) {
-    if (isNaN(e.key)) {
-        e.preventDefault();
-    }
-});
+// Adding event listeners for closing the modal
+document.querySelectorAll(".modal-background, button.delete")
+    .forEach((closeButton) => {
+        const modal = closeButton.closest(".my-modal");
+
+        closeButton.addEventListener("click", () => {
+            modal.classList.remove("is-active");
+            document.documentElement.classList.remove("is-clipped"); // Remove class from <html> element
+        });
+    });
